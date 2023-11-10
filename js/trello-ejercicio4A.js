9f527dee0e117d1975a79573173a8cbafced1ff3
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-11-10 16:19:21 -0300

    se realizo ejercicio4A de trello del tp3

---
+++ js/trello-ejercicio4A.js
@@ -0,0 +1,39 @@
+// Ejercicio 4A
+
+document.write(`<h1>Ejercicio 4A - Trello - Trabajo Practico 3</h1>`);
+
+function esPrimo(valor) {
+    if (valor < 2) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(valor); i++) {
+        if (valor % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function numerosPrimos(numero) {
+    if (Number.isInteger(numero)){
+        return false
+    }
+    const primos = [];
+    for (let i = 2; i <= numero; i++) {
+        if (esPrimo(i)) {
+            primos.push(i);
+        }
+    }
+    return primos;
+}
+
+
+const numero = parseInt(prompt('digite un numero entero'));
+const arraysPrimos = numerosPrimos(numero);
+
+document.write(`Los números primos hasta ${numero}: ${arraysPrimos}`);
+
+
+
+
+
